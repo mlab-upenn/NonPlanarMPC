da64d6298f118625bcc28c95bf9c6840c516dcdb
Ahmad Amine <ahmadamine998@gmail.com>
2025-11-11 13:53:07 -0500

    Add sim

---
+++ install_env.sh
@@ -0,0 +1,4 @@
+uv venv --python 3.11
+uv pip install --upgrade pip
+uv pip install "isaacsim[all,ros2,extscache]==5.1.0" --extra-index-url https://pypi.nvidia.com
+uv pip install -U torch==2.7.0 torchvision==0.22.0 --index-url https://download.pytorch.org/whl/cu128
---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+unset ROS_DISTRO AMENT_PREFIX_PATH COLCON_PREFIX_PATH PYTHONPATH RMW_IMPLEMENTATION
+export LD_LIBRARY_PATH=
+
+export ROS_DISTRO=humble
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ahmad/isaacsim/environments/nonPlanarSim/env_isaaclab/lib/python3.11/site-packages/isaacsim/exts/isaacsim.ros2.bridge/humble/lib
+
